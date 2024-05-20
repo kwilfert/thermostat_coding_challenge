6ccef080cfe3c2351c6e6b934dd920efed527325
kwilfert <kay.wilfert@gmail.com>
2024-05-20 12:37:02 +0200

    feat: thermostat scaffolding
    - create thermostat component
    - change app Icon
    - add inputs for temp control

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,36 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Thermostat from "@/components/Thermostat.vue";
+import {ref} from "vue";
+
+const minTemperature = ref(0);
+const maxTemperature = ref(0);
+const currentTemperature = ref(0);
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
+    <img alt="Thermometer App Logo" class="logo" height="125" src="./assets/thermostat_logo.png" width="125"/>
     <div class="wrapper">
-      <HelloWorld msg="Thermostat Coding Exercise" />
+      <HelloWorld msg="Thermostat Coding Exercise"/>
     </div>
   </header>
 
   <main>
-    <!-- Put thermostat component here -->
+    <h1>Thermostat Demonstration</h1>
+    <div>
+      <!-- temperature control inputs -->
+      <label for="minTemperature">Min. Temperature</label><br>
+      <input id="minTemperature" v-model="minTemperature" type="number"><br>
+      <label for="maxTemperature">Max. Temperature</label><br>
+      <input id="maxTemperature" v-model="maxTemperature" type="number"><br>
+      <label for="currentTemperature">Current Temperature</label><br>
+      <input id="currentTemperature" v-model="currentTemperature" type="number"><br>
+    </div>
+    <!-- Thermostat for display of values-->
+    <Thermostat :currentTemperature="currentTemperature"
+                :maxTemperature="maxTemperature"
+                :minTemperature="minTemperature"/>
   </main>
 </template>
 
---
+++ src/components/Thermostat.vue
@@ -0,0 +1,28 @@
+<script setup>
+// Props: maximumg, minimum and current temperature
+defineProps(
+    {
+      maxTemperature: {
+        type: Number,
+        required: true
+      },
+      minTemperature: {
+        type: Number,
+        required: true
+      },
+      currentTemperature: {
+        type: Number,
+        required: true
+      }
+    }
+)
+</script>
+<template>
+  <div>
+    <!-- thermostat -->
+  </div>
+</template>
+
+<style scoped>
+
+</style>
