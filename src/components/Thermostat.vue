0c6cf98d11c00c7949663e3efd65d1494adf0b9e
kwilfert <kay.wilfert@gmail.com>
2024-05-20 13:19:53 +0200

    feat: basic functionality
    - move indicator according to given parameters
    - no input validation, though

--- src/components/Thermostat.vue
+++ src/components/Thermostat.vue
@@ -3,8 +3,10 @@
 // Import SVG files as raw text
 import scale_svg_raw from '../assets/images/thermostat_scale.svg?raw';
 import indicator_svg_raw from '@/assets/images/thermostat_indicator.svg?raw';
+import {computed} from "vue";
 
-defineProps(
+// define relevant input values for thermostat component
+const props = defineProps(
     {
       maxTemperature: {
         type: Number,
@@ -20,11 +22,31 @@ defineProps(
       }
     }
 )
+
+// calculate indicator angle
+const indicatorAngle = computed(() => {
+  const {currentTemperature, minTemperature, maxTemperature} = props;
+  const percentage = (currentTemperature - minTemperature) / (maxTemperature - minTemperature);
+  return percentage * 180 - 90; // Convert to degrees (-90 to 90)
+});
+
+// set indicator style
+const indicatorStyle = computed(() => {
+  return {
+    transform: `rotate(${indicatorAngle.value}deg)`,
+    transformOrigin: 'center center',
+  };
+});
+
+
 </script>
 <template>
   <div class="thermostat-display">
+    <div class="temperature-display">{{ currentTemperature }}°</div>
     <div class="scale" v-html="scale_svg_raw"></div>
-    <div class="indicator" v-html="indicator_svg_raw"></div>
+    <div :style="indicatorStyle" class="indicator" v-html="indicator_svg_raw"></div>
+    <div class="min-temperature">{{ minTemperature }}°</div>
+    <div class="max-temperature">{{ maxTemperature }}°</div>
   </div>
 </template>
 
@@ -43,5 +65,19 @@ defineProps(
   left: 0;
   width: 100%;
   height: 100%;
+}
+
+.min-temperature {
+  position: absolute;
+  bottom: 10px;
+  left: 10px;
+  font-size: 12px;
+}
+
+.max-temperature {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  font-size: 12px;
 }
 </style>
