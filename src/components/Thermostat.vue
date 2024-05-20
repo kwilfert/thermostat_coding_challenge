b3022aec87344d002a6aacc8d8fe1ad7296201fb
kwilfert <kay.wilfert@gmail.com>
2024-05-20 13:00:52 +0200

    feat: show scale and indicator
    - shows scale and indicator in thermostat component
    - overlays them on top of each other

--- src/components/Thermostat.vue
+++ src/components/Thermostat.vue
@@ -1,5 +1,9 @@
 <script setup>
 // Props: maximumg, minimum and current temperature
+// Import SVG files as raw text
+import scale_svg_raw from '../assets/images/thermostat_scale.svg?raw';
+import indicator_svg_raw from '@/assets/images/thermostat_indicator.svg?raw';
+
 defineProps(
     {
       maxTemperature: {
@@ -18,11 +22,26 @@ defineProps(
 )
 </script>
 <template>
-  <div>
-    <!-- thermostat -->
+  <div class="thermostat-display">
+    <div class="scale" v-html="scale_svg_raw"></div>
+    <div class="indicator" v-html="indicator_svg_raw"></div>
   </div>
 </template>
 
 <style scoped>
+.thermostat-display {
+  position: relative;
+  width: 200px;
+  height: 200px;
+  text-align: center;
+}
 
+.scale,
+.indicator {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
 </style>
